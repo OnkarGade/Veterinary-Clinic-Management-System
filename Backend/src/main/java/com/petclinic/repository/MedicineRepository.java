84f54273a569e5d52169b1f9170122941a4d79ea
anubhavtewari <anubhav.tewari94@gmail.com>
2025-02-11 14:49:21 +0530

    updated appointment repository

--- Backend/src/main/java/com/petclinic/repository/MedicineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.petclinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.petclinic.pojos.Medicine;
-
-public interface MedicineRepository extends JpaRepository<Medicine, Long> {
-	
-}
